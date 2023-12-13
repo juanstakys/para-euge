f4bf9795c7524a3c52d4bfa4eab4f9409702c62c
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-12-12 22:43:51 -0300

    corrections

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="flex flex-col items-center select-none">
+  <div class="flex flex-col items-center select-none z-1">
     <h1 class="text-4xl mt-12">Hola Euge &lt;3</h1>
     <h2 :class="[siClickeado ? 'text-2xl text-warm-pink' : 'text-lg']">{{ msg }}</h2>
     <div>
-      <img src="./assets/loop-cute-love-cub.gif" alt="Cute love cub" draggable="false" />
+      <img
+        src="./assets/loop-cute-love-cub.gif"
+        alt="Cute love cub"
+        draggable="false"
+        class="z-2"
+      />
     </div>
     <div>
       <button
@@ -29,6 +34,7 @@
 
 <script setup>
 import { reactive, ref } from "vue";
+
 const offset = reactive({ top: "0px", left: "8px" });
 const msg = ref("¿Salimos a comer?");
 const siClickeado = ref(false);
@@ -48,7 +54,6 @@ function moveBtn(e) {
       -(window.innerHeight - e.clientY) / 2,
       (window.innerHeight - e.clientY) / 2
     ).toString() + "px";
-  console.log(offset);
 }
 
 function cambiarMsg() {
